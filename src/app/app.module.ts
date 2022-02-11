a36ca314451c87a3c2e7d16c2bb8038043fb69e4
Olya <ola.shy@yahoo.com>
2022-02-11 16:25:50 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import {AppComponent} from './app.component';
 import {NgxsModule} from '@ngxs/store';
 import {NgxsReduxDevtoolsPluginModule} from '@ngxs/devtools-plugin';
 import {NgxsLoggerPluginModule} from '@ngxs/logger-plugin';
+import {ListComponent} from './list/list.component';
+import {FormComponent} from './form/form.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
+import { TodoState } from './states/todo.state';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        ListComponent,
+        FormComponent
     ],
     imports: [
         BrowserModule,
-        NgxsModule.forRoot(),
+        NgxsModule.forRoot([
+          TodoState
+        ]),
         NgxsReduxDevtoolsPluginModule.forRoot(),
         NgxsLoggerPluginModule.forRoot(),
+        HttpClientModule,
+        ReactiveFormsModule,
         AppRoutingModule
     ],
     providers: [],
